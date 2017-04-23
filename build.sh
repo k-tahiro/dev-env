bc32f7e6068fa3440b9e3f872c5925384710832e
Keisuke Hirota <tahiro.k.ad@gmail.com>
2017-04-23 14:46:45 +0900

    remove space

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ if [ "NODEJS_VERSION" == "" ]; then
   NODEJS_VERSION="6.10.2"
 fi
 
-docker build --build-arg PYTHON_VERSION="${PYTHON_VERSION}"  --build-arg NODEJS_VERSION="${NODEJS_VERSION}" .
+docker build --build-arg PYTHON_VERSION="${PYTHON_VERSION}" --build-arg NODEJS_VERSION="${NODEJS_VERSION}" .
