424245c036ba0e7b17c16069735865c8cc36d9f9
Keisuke Hirota <tahiro.k.ad@gmail.com>
2017-04-23 15:53:11 +0900

    デフォルト値取得処理を変更

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-if [ "PYTHON_VERSION" == "" ]; then
-  PYTHON_VERSION="3.6.1"
-fi
-
-if [ "NODEJS_VERSION" == "" ]; then
-  NODEJS_VERSION="6.10.2"
-fi
+: ${PYTHON_VERSION:="3.6.1"}
+: ${NODEJS_VERSION:="6.10.2"}
 
 docker build --build-arg PYTHON_VERSION="${PYTHON_VERSION}" --build-arg NODEJS_VERSION="${NODEJS_VERSION}" .
